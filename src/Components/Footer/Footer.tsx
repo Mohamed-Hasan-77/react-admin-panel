a80e26c3af0fdb227f794418c5fa6fb4ca89454a
Mohamed-Hasan-77 <0mdx3210@gmail.com>
2023-10-19 19:52:52 +0200

    d

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -3,8 +3,7 @@
 export default function Footer() {
     return (
         <footer>
-
-            <div className="container flex justify-between items-center">
+            <div className="container flex justify-between items-center px-10 py-7">
                 <p className='font-bold'> Admin Dashboard  </p>
                 <p className='text-sm'> Made by &copy; Mohame Hasan </p>
             </div>
